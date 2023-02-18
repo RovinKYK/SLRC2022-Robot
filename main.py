67bc283eeef3637bdf51a98f1894cfc531dc1f9e
supungamlath <kykrovin@gmail.com>
2023-02-18 06:45:04 +0530

    Created basic file structure

---
+++ main.py
@@ -0,0 +1,13 @@
+from robot import Robot
+
+robot = Robot()
+
+#Helper functions for tasks
+
+########################################################################
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
