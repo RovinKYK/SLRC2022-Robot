171f52697251d21e039b33585d11b5d4911e3287
RovinKYK <kykrovin@gmail.com>
2023-02-22 00:11:58 +0530

    Added pwm control functions to motors

--- components.py
+++ components.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as IO
+import time
 from qmc5883l import *
+
 class Compass:
     def __init__(self):
         self.sensor = QMC5883L(output_data_rate=ODR_100HZ)
@@ -20,7 +22,7 @@ class Compass:
         return self.sensor.get_bearing()
 
 
-class IRsenor:
+class IRSensor:
     def __init__(self, pin):
         self.pin = pin
         IO.setwarnings(False)
@@ -30,3 +32,65 @@ class IRsenor:
     def detects_white(self):
         return not IO.input(self.pin)
 
+
+class Motor:
+    def __init__(self, en_pin, forward_pin, backward_pin):
+        self.en_pin = en_pin
+        self.forward_pin = forward_pin
+        self.backward_pin = backward_pin
+
+        self.frequency = 256
+        self.speed = 0.0
+        self.interval = 0.05
+        self.min_speed = 70
+
+        IO.setwarnings(False)
+        IO.setmode(IO.BOARD)
+        IO.setup(en_pin, IO.OUT)
+        IO.setup(forward_pin, IO.OUT)
+        IO.setup(backward_pin, IO.OUT)
+
+        self.pwm = IO.PWM(en_pin, self.frequency)
+        self.pwm.start(self.speed)
+
+    def move_forward_smooth(self, max_speed):
+        IO.output(self.forward_pin, IO.LOW)
+        IO.output(self.backward_pin, IO.HIGH)
+        
+        for speed in range(70,max_speed):
+            self.pwm.ChangeDutyCycle(speed)
+            self.speed = speed
+            time.sleep(self.interval)
+
+    def move_backward_smooth(self, max_speed):
+        IO.output(self.forward_pin, IO.HIGH)
+        IO.output(self.backward_pin, IO.LOW)
+        
+        for speed in range(70,max_speed):
+            self.pwm.ChangeDutyCycle(speed)
+            self.speed = speed
+            time.sleep(self.interval)
+
+    def stop_smooth(self):
+        for speed in range(speed,70,-1):
+            self.pwm.ChangeDutyCycle(speed)
+            self.speed = speed
+            time.sleep(self.interval)
+
+        self.stop()
+
+    def move_forward(self,speed):
+        IO.output(self.forward_pin, IO.LOW)
+        IO.output(self.backward_pin, IO.HIGH)
+        self.motorPWM.ChangeDutyCycle(speed)
+
+    def move_backward(self,speed):
+        IO.output(self.forward_pin, IO.HIGH)
+        IO.output(self.backward_pin, IO.LOW)
+        self.motorPWM.ChangeDutyCycle(speed)
+
+    def stop(self):
+        self.pwm.ChangeDutyCycle(0)
+
+    def shutdown(self):
+        self.pwm.stop()
